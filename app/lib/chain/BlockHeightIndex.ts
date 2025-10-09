35390d59257ef4a7d424b1cc5ef20fe5bf795cd5
Shiba <44804845+DeepDoge@users.noreply.github.com>
2025-10-09 07:34:50 +0000

    Refactor BlockHeightIndex to use BlockLocation from BlockPointer and move BlockLocation definition to a new file

--- app/lib/chain/BlockHeightIndex.ts
+++ app/lib/chain/BlockHeightIndex.ts
@@ -1,12 +1,5 @@
-import { Codec, Struct } from "@nomadshiba/codec";
 import { HeightStore } from "~/lib/chain/HeightStore.ts";
-import { u24 } from "~/lib/primitives/U24.ts";
-
-type BlockLocation = Codec.Infer<typeof BlockLocation>;
-const BlockLocation = new Struct({
-	chunkId: u24,
-	offset: u24,
-});
+import { BlockLocation } from "./primitives/BlockPointer.ts";
 
 export class BlockHeightIndex extends HeightStore<BlockLocation> {
 	constructor(path: string) {
